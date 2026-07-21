4a14c75476a9b776a9c0ae85567e789ff98845b4
BigBorne <shagtim152010@gmail.com>
2026-07-21 14:23:16 +0700

    Add docs

--- app/schemas.py
+++ app/schemas.py
@@ -2,17 +2,33 @@ from pydantic import BaseModel
 from typing import List
 
 class AdInput(BaseModel):
+    """
+    Схема тела запроса (JSON)
+    """
     itemId: int
     mcId: int
     mcTitle: str
     description: str
 
 class Draft(BaseModel):
+    """
+    В таком формате он предлагает нам черновик (JSON)
+    mcId: int - id объявления (предлагает сам)
+    mcTitle: str - название предложенного объявления
+    text: str - описание объявления
+    """
     mcId: int
     mcTitle: str
     text: str
 
 class ProcessingResponse(BaseModel):
+    """
+    Схема ответа от модели (status 200)
+    
+    detectedMcIds: List[int] - какие дополнительные объявления он заметил (список id)
+    shouldSplit: bool - нужно ли разделять объявление на разные категории (true/false)
+    drafts: List[Draft] = [] - список черновиков (из класса Draft)
+    """
     detectedMcIds: List[int]
     shouldSplit: bool
     drafts: List[Draft] = []
